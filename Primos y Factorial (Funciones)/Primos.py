7c8406a65c8b813715d56839ee4cc862d38768bc
AngeLeo2341 <142907969+AngeLeo2341@users.noreply.github.com>
2023-09-06 14:58:17 -0500

    Update Primos.py

--- Primos y Factorial (Funciones)/Primos.py
+++ Primos y Factorial (Funciones)/Primos.py
@@ -1,19 +1,20 @@
 print("Rango de Números Primos")
 
-while True:
+while True:   
+    def primo(x):
+        print('1 primo')
+        for i in range(2,x+1):
+            conta = 0
+            for n in range(2, x):
+                residue = i%n
+                if residue == 0:
+                    conta = conta + 1
+            if conta == 1:
+                        print(f'{i} primo')
+            else:
+                        print(f'{i} no primo')
     x=int(input('Ingrese el límite del rango: '))
-    print('1 primo')
-    for i in range(2,x+1):
-        conta = 0
-        for n in range(2, x):
-            residue = i%n
-            if residue == 0:
-                conta = conta + 1
-        if conta == 1:
-            
-            print(f'{i} primo')
-        else:
-            print(f'{i} no primo')
-    y=int(input("¿Desea finalizar el proceso? Digite 1 para confirmar: " ))
-    if y == 1:
+    print(primo(x))
+    y=int(input("Desea Terminar el proceso? Digite 1 para confirmar: "))
+    if y==1:
         break
